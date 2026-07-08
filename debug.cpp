6573abb53e6503ee043eff1516372114cd01cf95
Aaliyah Evans <amevans4@csuchico.edu>
2026-07-07 23:13:05 -0700

    Add files via upload
    Homework1

---
+++ debug.cpp
@@ -0,0 +1,83 @@
+/*
+There are eight (8) different syntax errors in this file
+Find each one and create a comment on the same line as the error,
+numbering it and explaining what the error was
+Ex: //ERROR 1: missing ;
+ */
+
+/this is my calculator!
+
+
+include <iostream>
+
+
+void greet(){
+    std::cout << "Welcome to my calc (that's short for calculator btw)!" << std::endl;
+    std::cout << "You can use the following operators:" << std::endl;
+    std::cout << "+\n-\n*\n/\n" << std::endl;
+
+
+int multiply(int a, int b){
+    return a * b;
+}
+
+
+int divide(int a, int b){
+    return a / b;
+}
+
+
+int addition(int a, int b){
+    return a + b;
+}
+
+
+int subtraction(int a, int b){
+    return a - b;
+}
+
+
+int math(int a, int b, char op){
+    switch(op){
+        case '+':
+            return addition(a, b);
+        case '-':
+            return subtraction(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            std::cout << "Invalid operator!" << std::endl;
+            return 0;
+    }
+}
+
+
+ main(){
+
+    int a;
+    int b;
+    char op
+
+    greet();
+
+    std::cout << "Give a number!" << std::endl;
+    std::cin << a;
+
+    std::cout << "Give me another number plz" << std::endl;
+    std::cin >> b;
+
+    std::cout << "What operator should be applied?" << endl;
+    std::cin >> op;
+
+    while (op != '+' && op != '-' && op != '*' && op != '/'){
+        std::cout << "That is not a valid operator, try again:" << std::endl;
+        std::cin >> op;
+    }
+
+    int answer == math(a, b, op);
+    std::cout << "Answer: " << answer << std::endl;
+
+    return 0;
+}
