42d7e9f5ff3bbeb20a2a792c97f046ca86ad8465
Aaliyah Evans <amevans4@csuchico.edu>
2026-07-09 15:08:07 -0700

    Refactor invalidChoice and price functions to use strings

--- fastfood.cpp
+++ fastfood.cpp
@@ -15,15 +15,15 @@ void menu(){
 }//end of menu func
 
 //this function forces the user to enter a valid menu choice
-int invalidChoice() {
-    int choice;
+std::string invalidChoice() {
+    std::string choice;
 
     //prompts user to enter valid input
     std::cout << "That is not a valid menu item. Try again: ";
     std::cin >> choice;
 
     //makes enter a valid input
-    while (choice < 1 || choice > 4) {
+    while (choice != "1" && choice != "2" && choice != "3" && choice != "4") {
         std::cout << "Still not valid. Enter a number between 1 and 4: ";
         std::cin >> choice;
     }//end of while
@@ -34,7 +34,7 @@ int invalidChoice() {
 float price(){
     float total = 0; //holds the total price of the user's order
     bool done = false; //tells the while loop when to stop
-    int choice; //holds user input
+    std::string choice; //holds user input
     char yN; //if the user is done ordering (Yes or No)
 
     while (!done){
@@ -43,29 +43,49 @@ float price(){
         std::cin >> choice;
 
         //if invalid, force correction BEFORE switch
-        if (choice < 1 || choice > 4){
+        if (choice != "1" && choice != "2" && choice != "3" && choice != "4"){
             choice = invalidChoice();
         }//end of if
 
         //based on user input, a certain amount of $$$ will be added to the total
-        switch (choice){
-            case 1:
-                std::cout << "10 Boneless Wings, yummersssss" << std::endl;
-                total += 13.50;
-                break;
-            case 2:
-                std::cout << "10 Bone-In, I guess we want to waste money today." << std::endl;
-                total += 14.99;
-                break;
-            case 3:
-                std::cout << "Voodoo Fries, omg those are my favorite" << std::endl;
-                total += 7.50;
-                break;
-            case 4:
-                std::cout << "A Cup of Ranch, don't worry I drink it too." << std::endl;
-                total += 5.00;
-                break;
-        }//end of switch
+        if (choice == "1"){
+            std::cout << "10 Boneless Wings, yummersssss" << std::endl;
+            total += 13.50;
+        }//end of if
+
+        else if (choice == "2"){
+            std::cout << "10 Bone-In, I guess we want to waste money today." << std::endl;
+            total += 14.99;
+        }//end of else if
+
+        else if (choice == "3"){
+            std::cout << "Voodoo Fries, omg those are my favorite" << std::endl;
+            total += 7.50;
+        }//end of els eif
+
+        else if (choice == "4"){
+            std::cout << "A Cup of Ranch, don't worry I drink it too." << std::endl;
+            total += 5.00;
+        }//end of else if
+
+        // switch (choice){
+        //     case 1:
+        //         std::cout << "10 Boneless Wings, yummersssss" << std::endl;
+        //         total += 13.50;
+        //         break;
+        //     case 2:
+        //         std::cout << "10 Bone-In, I guess we want to waste money today." << std::endl;
+        //         total += 14.99;
+        //         break;
+        //     case 3:
+        //         std::cout << "Voodoo Fries, omg those are my favorite" << std::endl;
+        //         total += 7.50;
+        //         break;
+        //     case 4:
+        //         std::cout << "A Cup of Ranch, don't worry I drink it too." << std::endl;
+        //         total += 5.00;
+        //         break;
+        // }//end of switch
 
         //prompts the user to add to their order
         std::cout << "Anything else? (Y/N): ";
